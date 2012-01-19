a52218582f40f5b5761584f86c48157a2f4a4e5c
Ryan Bonte <ryan.bonte@gmail.com>
2012-01-19 17:21:55 +0100

    deleted unused files

--- app/controllers/courses_controller.rb
+++
@@ -1,3 +0,0 @@
-class CoursesController < ApplicationController
-  
-end
--- app/models/tag.rb
+++
@@ -1,6 +0,0 @@
-class Tag
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  has_and_belongs_to_many :questions
-  belongs_to :category
-end
